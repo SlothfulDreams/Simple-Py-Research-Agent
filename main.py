81a533c909ea63bbf101bff27f5e347a824a3348
SlothfulDreams <bensonzheng87901@gmail.com>
2025-09-04 00:00:13 -0400

    agent

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
-load_dotenv()
+
+load_dotenv(".env.local")
+
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sourcesList: list[str]
+    tools_used: list[str]
+
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
@@ -11,3 +23,28 @@ llm = ChatGoogleGenerativeAI(
     timeout=None,
     max_retries=2,
 )
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+         You are a research assistant that will help generate a research paper.
+         Answer the user query and use neccessary tools.
+         Wrap the output in this format and provide no other text \n{format_instructions}
+         """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(llm=llm, prompt=prompt, tools=[])
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+raw_response = agent_executor.invoke({"query": "What is the capital of France"})
+
+structured_response = parser.parse(raw_response.get("output"))
+print(structured_response)
